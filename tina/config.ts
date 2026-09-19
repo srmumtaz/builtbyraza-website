91f7b66350a1f34b1e11064a35584939f1bf242f
srmumtaz <41485070+srmumtaz@users.noreply.github.com>
2026-09-19 08:30:25 -0400

    Remove employer badges from resume

--- tina/config.ts
+++ tina/config.ts
@@ -200,7 +200,6 @@ export default defineConfig({
             label: "Professional experience",
             list: true,
             fields: [
-              { type: "string", name: "logo", label: "Logo initials" },
               { type: "string", name: "employer", label: "Employer", required: true },
               { type: "string", name: "title", label: "Job title", required: true },
               { type: "string", name: "dates", label: "Dates", required: true },
